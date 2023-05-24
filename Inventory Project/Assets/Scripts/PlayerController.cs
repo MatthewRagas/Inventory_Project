a8698fedc53e93a79b25de994164233a3d8acec3
Matthew John Ragas <mjrag310@gmail.com>
2023-05-24 16:22:26 -0500

    Can now reduce inventory capacity
    Reducing Inventory Capacity will now remove items from the list and decrease Count and Capacity of list properly.

--- Inventory Project/Assets/Scripts/PlayerController.cs
+++ Inventory Project/Assets/Scripts/PlayerController.cs
@@ -22,15 +22,47 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         
-        //Change ivnentory Capacity
+        //Increase ivnentory Capacity
         if(Input.GetKeyDown(KeyCode.Period))
         {
             inventory.Container.Capacity += 1;
         }
 
+        /*Decrease inventory Capacity
+         *and drop items that no longer fit
+         */
         if(Input.GetKeyDown(KeyCode.Comma))
         {
-            inventory.Container.Capacity -= 1;
+            if(inventory.Container.Capacity == inventory.Container.Count)
+            {
+                int i = inventory.Container.Count - 1;
+                if(RemoveItem())
+                {
+                    inventory.Container.Capacity -= 1;
+                }                
+            }
+            else
+            {
+                inventory.Container.Capacity -= 1;
+            }            
         }
+    }
+
+    //Removes item at highest index
+    private bool RemoveItem()
+    {
+        bool itemRemoved = false;
+        //holds previous value of count
+        int previousCount = inventory.Container.Count;
+        inventory.Container.RemoveAt(inventory.Container.Count - 1);
+
+        //if count is now less than previousCount number
+        //then the item has been removed
+        if(previousCount > inventory.Container.Count)
+        {
+            //return true
+            itemRemoved = true;
+        }
+        return itemRemoved;
     }
 }
